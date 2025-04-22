537192e902964c5cb36a3337455eb61002b8f7f8
LunaRabbit66 <kt111558@gmail.com>
2025-04-23 00:00:43 +0900

    ラベルの表記ゆれを修正(末尾にコロンを付けるように統一)

--- src/SettingsForm.Designer.cs
+++ src/SettingsForm.Designer.cs
@@ -41,60 +41,67 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(30, 100);
+            this.label1.Location = new System.Drawing.Point(18, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(138, 18);
+            this.label1.Size = new System.Drawing.Size(94, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "Watching LogFile:";
             // 
             // textBoxWatchingFilePath
             // 
-            this.textBoxWatchingFilePath.Location = new System.Drawing.Point(33, 120);
+            this.textBoxWatchingFilePath.Location = new System.Drawing.Point(20, 80);
+            this.textBoxWatchingFilePath.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxWatchingFilePath.Multiline = true;
             this.textBoxWatchingFilePath.Name = "textBoxWatchingFilePath";
             this.textBoxWatchingFilePath.ReadOnly = true;
-            this.textBoxWatchingFilePath.Size = new System.Drawing.Size(725, 50);
+            this.textBoxWatchingFilePath.Size = new System.Drawing.Size(437, 35);
             this.textBoxWatchingFilePath.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(30, 10);
+            this.label2.Location = new System.Drawing.Point(18, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(105, 18);
+            this.label2.Size = new System.Drawing.Size(72, 12);
             this.label2.TabIndex = 2;
-            this.label2.Text = "LogDirectory";
+            this.label2.Text = "LogDirectory:";
             // 
             // textBoxLogDir
             // 
-            this.textBoxLogDir.Location = new System.Drawing.Point(33, 30);
+            this.textBoxLogDir.Location = new System.Drawing.Point(20, 20);
+            this.textBoxLogDir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxLogDir.Multiline = true;
             this.textBoxLogDir.Name = "textBoxLogDir";
-            this.textBoxLogDir.Size = new System.Drawing.Size(725, 50);
+            this.textBoxLogDir.Size = new System.Drawing.Size(437, 35);
             this.textBoxLogDir.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(30, 190);
+            this.label3.Location = new System.Drawing.Point(18, 127);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(174, 18);
+            this.label3.Size = new System.Drawing.Size(121, 12);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Discord Webhook URL";
+            this.label3.Text = "Discord Webhook URL:";
             // 
             // textBoxDiscordWebhookUrl
             // 
-            this.textBoxDiscordWebhookUrl.Location = new System.Drawing.Point(33, 210);
+            this.textBoxDiscordWebhookUrl.Location = new System.Drawing.Point(20, 140);
+            this.textBoxDiscordWebhookUrl.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxDiscordWebhookUrl.Multiline = true;
             this.textBoxDiscordWebhookUrl.Name = "textBoxDiscordWebhookUrl";
-            this.textBoxDiscordWebhookUrl.Size = new System.Drawing.Size(725, 50);
+            this.textBoxDiscordWebhookUrl.Size = new System.Drawing.Size(437, 35);
             this.textBoxDiscordWebhookUrl.TabIndex = 5;
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(654, 287);
+            this.buttonSave.Location = new System.Drawing.Point(392, 191);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(104, 37);
+            this.buttonSave.Size = new System.Drawing.Size(62, 25);
             this.buttonSave.TabIndex = 6;
             this.buttonSave.Text = "Save";
             this.buttonSave.UseVisualStyleBackColor = true;
@@ -102,9 +109,9 @@
             // 
             // SettingsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 349);
+            this.ClientSize = new System.Drawing.Size(480, 233);
             this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.textBoxDiscordWebhookUrl);
             this.Controls.Add(this.label3);
@@ -114,6 +121,7 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "SettingsForm";
             this.Text = "RNGNewAuraNotifier Settings";
