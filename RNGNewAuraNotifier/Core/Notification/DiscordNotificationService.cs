7eaae30a5b6a13e83c85cb7ed178c52ed3142ca3
Tomachi <8929706+book000@users.noreply.github.com>
2025-05-25 09:34:07 +0900

    fix: 未使用のNotifyオーバーロードの削除 (#53)
    Co-authored-by: LunaRabbit66 <102153621+LunaRabbit66@users.noreply.github.com>

--- RNGNewAuraNotifier/Core/Notification/DiscordNotificationService.cs
+++ RNGNewAuraNotifier/Core/Notification/DiscordNotificationService.cs
@@ -6,43 +6,11 @@ using Color = Discord.Color;
 
 namespace RNGNewAuraNotifier.Core.Notification;
 
+/// <summary>
+/// DiscordのWebhookを使用してメッセージを送信するサービス
+/// </summary>
 internal class DiscordNotificationService
 {
-    /// <summary>
-    /// DiscordのWebhookを使用してメッセージを送信する
-    /// </summary>
-    /// <param name="title">メッセージのタイトル</param>
-    /// <param name="message">メッセージの内容</param>
-    /// <param name="vrchatUser">VRChatユーザーの情報</param>
-    public static async Task NotifyAsync(string title, string message, VRChatUser? vrchatUser)
-    {
-        var url = AppConfig.DiscordWebhookUrl;
-        if (string.IsNullOrEmpty(url)) return;
-
-        using var client = new DiscordWebhookClient(url);
-        var embed = new EmbedBuilder
-        {
-            Title = title,
-            Description = message,
-            Footer = new EmbedFooterBuilder
-            {
-                Text = $"{AppConstants.AppName} {AppConstants.AppVersionString}",
-            },
-            Color = new Color(0x00, 0xFF, 0x00),
-            Timestamp = DateTimeOffset.UtcNow,
-        };
-        if (vrchatUser != null)
-        {
-            embed.Author = new EmbedAuthorBuilder
-            {
-                Name = vrchatUser.UserName,
-                Url = $"https://vrchat.com/home/user/{vrchatUser.UserId}",
-            };
-        }
-
-        await client.SendMessageAsync(text: "", embeds: [embed.Build()]).ConfigureAwait(false);
-    }
-
     /// <summary>
     /// DiscordのWebhookを使用してメッセージを送信する(Field形式)
     /// </summary>
