f5cfed5f87130cc5b2c245cd02fd5bc675f98eb0
LunaRabbit66 <kt111558@gmail.com>
2025-05-28 00:30:59 +0900

    設定プロパティの初期値と新規追加
    `LogDir` プロパティの初期値をVRChatのログディレクトリに変更し、環境変数を展開するようにしました。新たに `ToastNotification`（デフォルトは `true`）、`WindowsStartup`（デフォルトは `false`）、および `ConfigDir` プロパティを追加しました。これにより、トースト通知の制御やWindowsスタートアップの設定、設定ファイルのディレクトリパスの管理が可能になりました。

--- RNGNewAuraNotifier/Core/Config/ConfigData.cs
+++ RNGNewAuraNotifier/Core/Config/ConfigData.cs
@@ -13,11 +13,31 @@ internal class ConfigData
     /// </summary>
     /// <remarks>デフォルトは %USERPROFILE%\AppData\LocalLow\VRChat\VRChat</remarks>
     [JsonPropertyName("logDir")]
-    public string LogDir { get; set; } = string.Empty;
+    public string LogDir { get; set; } = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\LocalLow\VRChat\VRChat");
 
     /// <summary>
     /// DiscordのWebhook URL
     /// </summary>
     [JsonPropertyName("discordWebhookUrl")]
     public string DiscordWebhookUrl { get; set; } = string.Empty;
+
+    /// <summary>
+    /// トースト通知の有効/無効
+    /// </summary>
+    /// <remarks>デフォルトは true(有効)</remarks>
+    [JsonPropertyName("toastNotification")]
+    public bool ToastNotification { get; set; } = true;
+
+    /// <summary>
+    /// Windowsスタートアップの有効/無効
+    /// </summary>
+    /// <remarks>デフォルトは false(無効)</remarks>
+    [JsonPropertyName("windowsStartup")]
+    public bool WindowsStartup { get; set; } = false;
+
+    /// <summary>
+    /// 設定ファイルのディレクトリのパス
+    /// </summary>
+    [JsonPropertyName("configDir")]
+    public string ConfigDir { get; set; } = Environment.ExpandEnvironmentVariables(@$"%USERPROFILE%\AppData\Local\{AppConstants.AppName}");
 }
