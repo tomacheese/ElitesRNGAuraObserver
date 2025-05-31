232f5ad67e1dff48ae5e37b0c999cf9c0039c0bc
LunaRabbit66 <102153621+LunaRabbit66@users.noreply.github.com>
2025-06-01 00:11:33 +0900

    feat: アプリケーション/プロジェクト名の変更 (#63)
    * アプリケーション名変更対応
    
    - プロジェクト名、ディレクトリ名の変更
    - 名前空間、クラス名、定数の一部修正
    
    * アプリケーション名の変更と名前空間の追加
    
    `AppConstants.cs` でアプリケーション名を `AppName` から `AssemblyName` に変更し、表示名 `DisplayAppName` を追加しました。
    `ElitesRNGAuraObserver.Updater.csproj` で `AssemblyName` を `$(AssemblyName)` に変更し、`Product` プロパティを追加しました。
    `RegistryManager.cs` と `DiscordNotificationService.cs` で、レジストリや通知に表示されるアプリケーション名を更新しました。
    `Program.cs` でも環境情報に表示されるアプリケーション名を変更しました。
    `SettingsForm.Designer.cs` と `TrayIcon.cs` に `ElitesRNGAuraObserver.Core` 名前空間を追加し、トレイアイコンのテキストも更新しました。
    
    * workfrowsの修正
    アセンブリ名称の修正
    DisplayNameの修正
    SettingsFormのレイアウトを修正
    
    * 改行の削除

---
+++ ElitesRNGAuraObserver/Core/Aura/Aura.cs
@@ -0,0 +1,128 @@
+using ElitesRNGAuraObserver.Core.Json;
+
+namespace ElitesRNGAuraObserver.Core.Aura;
+
+/// <summary>
+/// Auraの情報を表すレコード
+/// </summary>
+internal record Aura
+{
+    /// <summary>
+    /// Aura の ID
+    /// </summary>
+    /// <example>60</example>
+    public int Id { get; init; }
+
+    /// <summary>
+    /// Aura の名前
+    /// </summary>
+    /// <example>Celebration</example>
+    public string? Name { get; init; }
+
+    /// <summary>
+    /// オーラの当選確率
+    /// </summary>
+    /// <example>1000000</example>
+    public int Rarity { get; init; }
+
+    /// <summary>
+    /// オーラのティア
+    /// </summary>
+    /// <remarks>
+    /// Rarityの高さによる区分け、ゲーム内の演出をもとに割り振っている。
+    ///
+    /// Rarity: ～ 999 Tier:5
+    /// Rarity: 1000 ～ 9999 Tier:4
+    /// Rarity: 10000 ～ 99999 Tier:3
+    /// Rarity: 100000 ～ 999999 Tier:2
+    /// Rarity: 1000000 ～ 9999999 Tier:1
+    /// SPECIAL枠のAura(特殊な入手条件のAuraのみ)はTier:0
+    /// </remarks>
+    /// <example>4</example>
+    public int Tier { get; init; }
+
+    /// <summary>
+    /// オーラのサブテキスト
+    /// </summary>
+    /// <example>VALENTINE’S EXCLUSIVE</example>
+    public string SubText { get; init; } = string.Empty;
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="id">Aura の ID</param>
+    /// <param name="name">Aura の名前</param>
+    /// <param name="rarity">オーラの当選確率</param>
+    /// <param name="tier">オーラのティア</param>
+    /// <param name="subText">オーラのサブテキスト</param>
+    public Aura(int id, string? name = null, int rarity = 0, int tier = 0, string subText = "")
+    {
+        Id = id;
+        Name = name;
+        Rarity = rarity;
+        Tier = tier;
+        SubText = subText;
+    }
+
+    /// <summary>
+    /// Aura を取得する
+    /// </summary>
+    /// <param name="auraId">Aura の ID</param>
+    /// <returns>Aura のインスタンス</returns>
+    public static Aura GetAura(int auraId)
+    {
+        try
+        {
+            // JSONをAura[]にデシリアライズ
+            Aura[] auras = JsonData.GetAuras();
+
+            return auras.FirstOrDefault(aura => aura.Id == auraId) ?? new Aura(auraId);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error deserializing Auras({ex.GetType().Name}): {ex.Message}");
+            return new Aura(auraId);
+        }
+    }
+
+    /// <summary>
+    /// オーラの名前を取得する
+    /// </summary>
+    /// <remarks>
+    /// subTextがnullの場合、Nameをそのまま返す。
+    /// subTextが存在する場合、NameとsubTextを括弧で囲んで結合する。
+    /// </remarks>
+    /// <example>
+    /// "Event Horizon"
+    /// "Cupid (VALENTINE’S EXCLUSIVE)"
+    /// </example>
+    /// <returns>通知に表示するオーラ名称</returns>
+    public string? GetNameText() => string.IsNullOrEmpty(SubText) ? Name : $"{Name} ({SubText})";
+
+    /// <summary>
+    /// オーラのレアリティを取得する
+    /// </summary>
+    /// <remarks>
+    /// Rarityが0では無い場合、"1 in"の後にレアリティの数値をカンマ区切りで表示。
+    /// Rarityが0の場合、"???"を表示。
+    /// </remarks>
+    /// <example>
+    /// "1 in 1,000,000"
+    /// "???"
+    /// </example>
+    /// <returns>通知に表示するレアリティ</returns>
+    public string GetRarityString() => Rarity != 0 ? $"1 in {Rarity:N0}" : "???";
+
+    /// <summary>
+    /// オーラの等価性を比較する
+    /// </summary>
+    /// <param name="other">比較対象のAura</param>
+    /// <returns>等価であればtrue、そうでなければfalse</returns>
+    public virtual bool Equals(Aura? other) => other != null && Id == other.Id;
+
+    /// <summary>
+    /// オーラのハッシュコードを取得する
+    /// </summary>
+    /// <returns>ハッシュコード</returns>
+    public override int GetHashCode() => Id.GetHashCode();
+}
---
+++ ElitesRNGAuraObserver/Core/Notification/UwpNotificationService.cs
@@ -0,0 +1,22 @@
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace ElitesRNGAuraObserver.Core.Notification;
+
+/// <summary>
+/// Windowsのトースト通知を表示するサービス
+/// </summary>
+internal static class UwpNotificationService
+{
+    /// <summary>
+    /// Windowsのトースト通知を表示する
+    /// </summary>
+    /// <param name="title">通知のタイトル</param>
+    /// <param name="message">通知のメッセージ</param>
+    public static void Notify(string title, string message)
+    {
+        new ToastContentBuilder()
+            .AddText(title)
+            .AddText(message)
+            .Show();
+    }
+}
