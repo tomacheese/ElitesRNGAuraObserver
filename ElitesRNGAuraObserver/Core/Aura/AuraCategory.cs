614d63c09b15459e7f425ee35deef8d831bc1660
LunaRabbit66 <kt111558@gmail.com>
2026-01-04 20:43:29 +0900

    AuraCategoryをenum形式に変更

--- ElitesRNGAuraObserver/Core/Aura/AuraCategory.cs
+++ ElitesRNGAuraObserver/Core/Aura/AuraCategory.cs
@@ -1,76 +1,67 @@
 namespace ElitesRNGAuraObserver.Core.Aura;
 
 /// <summary>
-/// Auraのカテゴリを表すクラス
+/// Auraのカテゴリを表す列挙体
 /// </summary>
-internal class AuraCategory
+internal enum AuraCategory
 {
     /// <summary>
-    /// Auraのカテゴリ定義
+    /// Aura情報が取得できない場合に設定されます。
     /// </summary>
-    internal readonly struct AuraCategories
-    {
-        /// <summary>
-        /// UnknownカテゴリのID
-        /// </summary>
-        /// <remarks>
-        /// Aura情報が取得できない場合に設定されます。
-        /// </remarks>
-        public const int Unknown = 0;
+    Unknown = 0,
 
-        /// <summary>
-        /// OrdinaryカテゴリのID
-        /// </summary>
-        public const int Ordinary = 1;
+    /// <summary>
+    /// Ordinaryカテゴリ
+    /// </summary>
+    Ordinary = 1,
 
-        /// <summary>
-        /// RefinedカテゴリのID
-        /// </summary>
-        public const int Refined = 2;
+    /// <summary>
+    /// Refinedカテゴリ
+    /// </summary>
+    Refined = 2,
 
-        /// <summary>
-        /// EnhancedカテゴリのID
-        /// </summary>
-        public const int Enhanced = 3;
+    /// <summary>
+    /// Enhancedカテゴリ
+    /// </summary>
+    Enhanced = 3,
 
-        /// <summary>
-        /// AdvancedカテゴリのID
-        /// </summary>
-        public const int Advanced = 4;
+    /// <summary>
+    /// Advancedカテゴリ
+    /// </summary>
+    Advanced = 4,
 
-        /// <summary>
-        /// FormidableカテゴリのID
-        /// </summary>
-        public const int Formidable = 5;
+    /// <summary>
+    /// Formidableカテゴリ
+    /// </summary>
+    Formidable = 5,
 
-        /// <summary>
-        /// CatastrophicカテゴリのID
-        /// </summary>
-        public const int Catastrophic = 6;
+    /// <summary>
+    /// Catastrophicカテゴリ
+    /// </summary>
+    Catastrophic = 6,
 
-        /// <summary>
-        /// AnnihilatoryカテゴリのID
-        /// </summary>
-        public const int Annihilatory = 7;
+    /// <summary>
+    /// Annihilatoryカテゴリ
+    /// </summary>
+    Annihilatory = 7,
 
-        /// <summary>
-        /// ExclusiveカテゴリのID
-        /// </summary>
-        public const int Exclusive = 8;
+    /// <summary>
+    /// Exclusiveカテゴリ
+    /// </summary>
+    Exclusive = 8,
 
-        /// <summary>
-        /// ValentinesカテゴリのID
-        /// </summary>
-        public const int Valentines = 9;
+    /// <summary>
+    /// Valentinesカテゴリ
+    /// </summary>
+    Valentines = 9,
 
-        /// <summary>
-        /// HalloweenカテゴリのID
-        /// </summary>
-        public const int Halloween = 10;
+    /// <summary>
+    /// Halloweenカテゴリ
+    /// </summary>
+    Halloween = 10,
 
-        /// <summary>
-        /// ChristmasカテゴリのID
-        /// </summary>
-        public const int Christmas = 11;
-    }
+    /// <summary>
+    /// Christmasカテゴリ
+    /// </summary>
+    Christmas = 11,
 }
