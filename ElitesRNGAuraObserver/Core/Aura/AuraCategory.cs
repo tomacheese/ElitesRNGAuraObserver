f0eff6905e8627cc4effffc9d3bc4c44d72e0586
LunaRabbit66 <kt111558@gmail.com>
2026-01-04 17:09:12 +0900

    feat: Chrismas Updateで追加された新規オーラへの対応 一部オーラの確率誤表記を修正。 JSONフォーマットを修正、TierをCategoryに変更し、Special枠のオーラであるかのフラグを追加

--- ElitesRNGAuraObserver/Core/Aura/AuraCategory.cs
+++ ElitesRNGAuraObserver/Core/Aura/AuraCategory.cs
@@ -11,58 +11,66 @@ internal class AuraCategory
     internal readonly struct AuraCategories
     {
         /// <summary>
+        /// UnknownカテゴリのID
+        /// </summary>
+        /// <remarks>
+        /// Aura情報が取得できない場合に設定されます。
+        /// </remarks>
+        public const int Unknown = 0;
+
+        /// <summary>
         /// OrdinaryカテゴリのID
         /// </summary>
-        public const int Ordinary = 0;
+        public const int Ordinary = 1;
 
         /// <summary>
         /// RefinedカテゴリのID
         /// </summary>
-        public const int Refined = 1;
+        public const int Refined = 2;
 
         /// <summary>
         /// EnhancedカテゴリのID
         /// </summary>
-        public const int Enhanced = 2;
+        public const int Enhanced = 3;
 
         /// <summary>
         /// AdvancedカテゴリのID
         /// </summary>
-        public const int Advanced = 3;
+        public const int Advanced = 4;
 
         /// <summary>
         /// FormidableカテゴリのID
         /// </summary>
-        public const int Formidable = 4;
+        public const int Formidable = 5;
 
         /// <summary>
         /// CatastrophicカテゴリのID
         /// </summary>
-        public const int Catastrophic = 5;
+        public const int Catastrophic = 6;
 
         /// <summary>
         /// AnnihilatoryカテゴリのID
         /// </summary>
-        public const int Annihilatory = 6;
+        public const int Annihilatory = 7;
 
         /// <summary>
         /// ExclusiveカテゴリのID
         /// </summary>
-        public const int Exclusive = 7;
+        public const int Exclusive = 8;
 
         /// <summary>
         /// ValentinesカテゴリのID
         /// </summary>
-        public const int Valentines = 8;
+        public const int Valentines = 9;
 
         /// <summary>
         /// HalloweenカテゴリのID
         /// </summary>
-        public const int Halloween = 9;
+        public const int Halloween = 10;
 
         /// <summary>
         /// ChristmasカテゴリのID
         /// </summary>
-        public const int Christmas = 10;
+        public const int Christmas = 11;
     }
 }
