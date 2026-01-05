fb974f909e217702ff39f1abd42e01b6a89fc9df
LunaRabbit66 <102153621+LunaRabbit66@users.noreply.github.com>
2026-01-05 19:56:39 +0900

    feat: Christmas Update対応 (#89)

---
+++ ElitesRNGAuraObserver/Core/Aura/AuraCategory.cs
@@ -0,0 +1,67 @@
+namespace ElitesRNGAuraObserver.Core.Aura;
+
+/// <summary>
+/// Auraのカテゴリを表す列挙体
+/// </summary>
+internal enum AuraCategory
+{
+    /// <summary>
+    /// Aura情報が取得できない場合に設定されます。
+    /// </summary>
+    Unknown = 0,
+
+    /// <summary>
+    /// Ordinaryカテゴリ
+    /// </summary>
+    Ordinary = 1,
+
+    /// <summary>
+    /// Refinedカテゴリ
+    /// </summary>
+    Refined = 2,
+
+    /// <summary>
+    /// Enhancedカテゴリ
+    /// </summary>
+    Enhanced = 3,
+
+    /// <summary>
+    /// Advancedカテゴリ
+    /// </summary>
+    Advanced = 4,
+
+    /// <summary>
+    /// Formidableカテゴリ
+    /// </summary>
+    Formidable = 5,
+
+    /// <summary>
+    /// Catastrophicカテゴリ
+    /// </summary>
+    Catastrophic = 6,
+
+    /// <summary>
+    /// Annihilatoryカテゴリ
+    /// </summary>
+    Annihilatory = 7,
+
+    /// <summary>
+    /// Exclusiveカテゴリ
+    /// </summary>
+    Exclusive = 8,
+
+    /// <summary>
+    /// Valentinesカテゴリ
+    /// </summary>
+    Valentines = 9,
+
+    /// <summary>
+    /// Halloweenカテゴリ
+    /// </summary>
+    Halloween = 10,
+
+    /// <summary>
+    /// Christmasカテゴリ
+    /// </summary>
+    Christmas = 11,
+}
